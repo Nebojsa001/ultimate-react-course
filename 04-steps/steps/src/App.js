4afe19ec9871e5b9cf66f8658e0f7f4a98b0fbdb
Nebojsa Trivunovic <ntrivunovic75@gmail.com>
2025-08-04 22:21:44 +0200

    finished s6-e60 - useState

---
+++ 04-steps/steps/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 && "active"}>1</div>
+        <div className={step >= 2 && "active"}>2</div>
+        <div className={step >= 3 && "active"}>3</div>
+      </div>
+      <div className="message">
+        Step{step}: {messages[step - 1]}
+      </div>
+      <div className="buttons">
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
